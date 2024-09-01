c92dfd6cda707bd4aa7cac7531685c52fda600a9
RajalakshmiGiri18 <rajalakshmigiri18@gmail.com>
2024-09-01 07:12:49 -0700

    Drag and Drop Concept

---
+++ Drag and Drop concept/src/com/dragdrop/DragDropExample.java
@@ -0,0 +1,23 @@
+package com.dragdrop;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import utilityDrivers.Helper;
+
+public class DragDropExample {
+
+	public static void main(String[] args) throws Exception {
+		WebDriver driver = Helper.startBrowser("Chrome");
+		driver.get("http://demo.guru99.com/test/drag_drop.html");
+		Thread.sleep(2000);
+		
+		Actions a = new Actions(driver);
+		WebElement source = driver.findElement(By.xpath("//a[text()=' BANK ']"));
+		WebElement des = driver.findElement(By.xpath("(//li[@class='placeholder'])[1]"));
+		Thread.sleep(2000);
+		a.clickAndHold(source).moveToElement(des).release(des).perform();
+	}
+}
